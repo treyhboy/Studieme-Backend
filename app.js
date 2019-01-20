a48337c56b852b040cc1fe9f13aa16dd9797af23
treyhboy <taruntrehen@gmail.com>
2019-01-21 04:18:04 +0530

    V1

---
+++ app.js
@@ -0,0 +1,16 @@
+// var x = 1
+// setTimeout(()=>{x=0;},3000)
+// while(x)
+// {
+//     console.log("hello")
+// }
+var k = 1
+while(k) {
+    var x = 1;
+    console.log("hello")
+    k=0
+}
+
+console.log(x)
+
+
